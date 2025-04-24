6c198889e297d06918c3f54dcb2ffbe40e4733d3
Jayde Ver Elst <45283009+Basjohn@users.noreply.github.com>
2025-04-24 02:51:20 +0200

    Delete release_scripts directory

--- release_scripts/macos/build_with_pyinstaller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# MacOS: Build standalone executable with PyInstaller
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-pip install pyinstaller
-pyinstaller --distpath dist/macos_onefile --workpath build/macos_onefile -y --onefile src/main.py --name=shitty-timezone-converter --noconsole
