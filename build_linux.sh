7f86289095e7070b557ffeec84bff7315fc48dfe
Jayde Ver Elst <45283009+Basjohn@users.noreply.github.com>
2025-04-24 03:00:45 +0200

    Add files via upload

---
+++ build_linux.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pyinstaller --onefile --windowed --add-data "Moist:Moist" main.py
---
+++ build_windows.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pyinstaller --onefile --windowed --icon=poopicon.ico --add-data "Moist:Moist" main.py
